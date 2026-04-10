03ebd924ecfe9907d04038b1c25c789863392304
Thiru <thiru@waioz.com>
2026-04-10 19:03:50 +0530

    gradle run

--- lib/core/constants/app_config.dart
+++ lib/core/constants/app_config.dart
@@ -1,4 +1,5 @@
 class AppConfig {
 
-  static const String baseUrl = 'http://192.168.0.122:3000/';
+  //static const String baseUrl = 'http://192.168.0.122:3000/';
+  static const String baseUrl = 'https://api.dev.padel.waioz.com/';
 }
